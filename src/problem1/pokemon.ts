c0230ddc63a1452f812a614c3e51b3e05a310640
Micaela Lucia Mungay Juncal <alu0101124506@ull.edu.es>
2021-03-21 00:50:42 +0000

    Final primer ejercicio

---
+++ src/problem1/pokemon.ts
@@ -0,0 +1,23 @@
+export class Pokemon{
+
+    public name: string;
+    public weight: number;
+    public height: number;
+    public type_: string;
+    public attack: number;
+    public defense: number;
+    public speed: number; 
+    public hp: number;
+
+    constructor(name: string, weight: number, height: number, type_: string, attack: number, defense: number, speed: number, hp: number){
+        this.name = name;
+        this.weight = weight;
+        this.height = height;
+        this.type_ = type_;
+        this.attack = attack;
+        this.defense = defense;
+        this.speed = speed;
+        this.hp = hp;
+    }
+
+}
--- src/problem1/problem1.ts
+++ src/problem1/problem1.ts
@@ -1,26 +1,5 @@
-class Pokemon{
+import { Pokemon } from './pokemon';
 
-    public name: string;
-    public weight: number;
-    public height: number;
-    public type_: string;
-    public attack: number;
-    public defense: number;
-    public speed: number; 
-    public hp: number;
-
-    constructor(name: string, weight: number, height: number, type_: string, attack: number, defense: number, speed: number, hp: number){
-        this.name = name;
-        this.weight = weight;
-        this.height = height;
-        this.type_ = type_;
-        this.attack = attack;
-        this.defense = defense;
-        this.speed = speed;
-        this.hp = hp;
-    }
-
-}
 
 let ninetales = new Pokemon("ninetales", 19.9, 1.1, "fuego", 76, 75, 100, 73);
 let leafeon = new Pokemon("leafeon", 25.5, 1.0, "hierba", 110, 130, 95, 65);
