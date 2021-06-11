0b6567f7e71ef984ce3a02c1ea405990da41daa8
Micaela Lucia Mungay Juncal <alu0101124506@ull.edu.es>
2021-06-11 02:03:16 +0100

    hecho

--- tests/problem1.spec.ts
+++ tests/problem1.spec.ts
@@ -2,7 +2,7 @@ import 'mocha';
 import {expect} from 'chai';
 import {Combat} from '../src/problem1/problem1';
 import {Pokemon} from '../src/problem1/problem1-1';
-//uaooeace
+
 let ninetales = new Pokemon("ninetales", 19.9, 1.1, "fuego", 76, 75, 100, 73);
 let leafeon = new Pokemon("leafeon", 25.5, 1.0, "hierba", 110, 130, 95, 65);
 
